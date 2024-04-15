8dc77a7983ef38c4378eac872293aa5dfe5db502
Gosia Basińska <31844953+odmina@users.noreply.github.com>
2024-04-15 10:38:35 +0200

    minor changes (removed unused package, formatting)

--- OU_process_2v.py
+++ OU_process_2v.py
@@ -1,5 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
 from scipy.linalg import expm
 
 
@@ -15,7 +14,9 @@ class OU_process_2v(object):
         the process, it is asserted to be a real matrix with
         with positive eigenvalues.
     `Gamma` - stationary covariance matrix of the OU process;
-    `mu` - mean of the process, by default 0, but can be changed.
+
+    During data simulation parameter `mu` may be provided.
+        It is the mean of the process, it is 0 by default.
 
     The simulation uses discrete timesteps of equal length d
         and at each timestep samples X(t) from a conditional
@@ -29,10 +30,10 @@ class OU_process_2v(object):
 
     Cholesky decomposition of instantaneus covariance (difussion)
         matrix appears in the stochastic term of the equation of X(t).
-        For the simulation, the conditional distribution of X(t) 
+        For the simulation, the conditional distribution of X(t)
         given X(t-d) is reparametrized so that it depends on B and Gamma.
         Thus the following condition has to be satisfied:
-        Sigma = B*Gamma + Gamma*B.T 
+        Sigma = B*Gamma + Gamma*B.T
         is a Hermitian, positive-definite marix.
         Since for the simulation only real valued matrices are used,
         Sigma has to be symetric.
@@ -44,10 +45,9 @@ class OU_process_2v(object):
         distribution with mean mu and covariance matrix Gamma.
     """
 
-    def __init__(self, B, Gamma, mu=[0, 0]):
+    def __init__(self, B, Gamma):
         self.B = np.array(B)
         self.Gamma = np.array(Gamma)
-        self.mu = np.array(mu)
         # B is assumed to be 2x2, real and have positive eigenvalues
         assert np.all(np.isreal(self.B)), "B is not a real matrix"
         assert self.B.shape == (2, 2), "B is not 2x2 matrix"
@@ -68,24 +68,23 @@ class OU_process_2v(object):
         assert np.all(self.Sigma == self.Sigma.T) and \
             np.all(np.linalg.eigvals(self.Sigma) > 0), \
             "Diffusion matrix not possitive-definite"
-        # mu nas to be a vector of length 2
-        assert self.mu.shape == (2,), "mu is not (2,) array"
 
     def __str__(self):
         return """Two variable Ornstein–Uhlenbeck process;
-        `use get_B()`, `get_Gamma()` and 'get_mu()' to print parameters. """
+        `use get_B()`, `get_Gamma()` and 'get_Sigma()' to access parameters."""
+
+    # ------ GETTERS ------
 
     def get_B(self):
-        return (self.B)
+        return self.B
 
     def get_Gamma(self):
-        return (self.Gamma)
+        return self.Gamma
 
     def get_Sigma(self):
-        return (self.Sigma)
+        return self.Sigma
 
-    def get_mu(self):
-        return (self.mu)
+    # ------ DISCRETE TIME SIMULATION METHODS ------
 
     def get_sim_discreteB(self, d=0.1):
         """
@@ -99,24 +98,26 @@ class OU_process_2v(object):
         """
         Calculates the variance of the distribution of X(t) given X(t-d).
 
-        Uses timestep `d`, with default 0.1. 
+        Uses timestep `d`, with default 0.1.
 
-        The covariance is determined by the stochastic term of the 
-        X(t) equation describing the process. 
-        Cholesky decomposition of instantaneus covariance (difussion) 
-        matrix appears in this equation. The conditional distribution 
+        The covariance is determined by the stochastic term of the
+        X(t) equation describing the process.
+        Cholesky decomposition of instantaneus covariance (difussion)
+        matrix appears in this equation. The conditional distribution
         of X(t) given X(t-d) is reparametrized so that it depends
         on B and Gamma.See for example oravecz2011hierarchical eq (5).
         """
+
         cov = self.Gamma - np.matmul(
             np.matmul(expm(-self.B*d), self.Gamma),
             expm(-self.B.T*d)
         )
         return cov
 
-    def sim_data(self, d=0.1, total_time=10):
+    def sim_data(self,  mu=[0, 0], d=0.1, total_time=10):
         """
-        This function numerically simulates a two vairable Ornstein–Uhlenbeck process.
+        This function simulates a two vairable Ornstein–Uhlenbeck process.
+
         Let vector X(t) be a vector of two variables at time t.
         Covariance matrix of the OU process is `Gamma` (and is stationary).
         Mean of the process `mu` is by default 0, but can be changed.
@@ -134,6 +135,8 @@ class OU_process_2v(object):
             distribution with mean mu and covariance matrix Gamma.
         """
 
+        assert mu.shape == (2,), "mu is not (2,) array"
+
         rng = np.random.default_rng()
 
         # initialize data array
@@ -145,8 +148,11 @@ class OU_process_2v(object):
         cond_B = self.get_sim_discreteB(d=d)
 
         # set process start X(t=0)
-        X[:, 0] = rng.multivariate_normal(self.mu, self.Gamma)
+        X[:, 0] = rng.multivariate_normal(mu, self.Gamma)
+
+        # simulate data
         for i in np.arange(n_datapoints - 1):
-            this_mu = self.mu + cond_B*(X[:, i] - self.mu)
+            this_mu = mu + cond_B*(X[:, i] - mu)
             X[:, i + 1] = rng.multivariate_normal(this_mu, condPDF_cov)
+
         return X
