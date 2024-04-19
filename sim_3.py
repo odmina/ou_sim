45c4f2bda232ba6320ce6b3f8daee4bb972d2813
Gosia Basińska <31844953+odmina@users.noreply.github.com>
2024-04-19 09:03:10 +0200

    added stubs for simulation

---
+++ sim_3.py
@@ -0,0 +1,23 @@
+import numpy as np
+import OU_process_2v
+import person
+
+
+####################################################################################################
+# 3: If predictiors are measured some time before the resul, the accuracy of predictions is affected
+# by the time interval between measurement occasions
+####################################################################################################
+
+####################################################################################################
+# 3.1: In the absence of individual differences the accuracy of predictions goes to 0 over the time
+# interval deterimined by the cross-effect of the predictor on dependent variable (this can be
+# calculated, put the calculation in the suplement)
+####################################################################################################
+
+
+
+####################################################################################################
+# 3.2: When inter-individual differences are present, predictions accuracy goes to the level
+# determined by the correlation of central tendencies of both variables
+####################################################################################################
+
---
+++ sim_4.py
@@ -0,0 +1,8 @@
+import numpy as np
+import OU_process_2v
+import person
+
+
+####################################################################################################
+# 4: People may differ in their intra - individual variances and in how strongly they respond
+####################################################################################################
