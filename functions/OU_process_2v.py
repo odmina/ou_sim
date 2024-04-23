5072e928e586ab9c3018bfa6e4b41806a99bf914
Gosia Basińska <31844953+odmina@users.noreply.github.com>
2024-04-23 13:51:01 +0200

    OU process can be initialized with Gamma or Sigma

--- functions/OU_process_2v.py
+++ functions/OU_process_2v.py
@@ -28,46 +28,77 @@ class OU_process_2v(object):
         with 1/'d' samples per unit.
     The function uses a conditional distribution of X(t).
 
-    Cholesky decomposition of instantaneus covariance (difussion)
+    Cholesky decomposition of instantaneus covariance
         matrix appears in the stochastic term of the equation of X(t).
         For the simulation, the conditional distribution of X(t)
         given X(t-d) is reparametrized so that it depends on B and Gamma.
         Thus the following condition has to be satisfied:
         Sigma = B*Gamma + Gamma*B.T
         is a Hermitian, positive-definite marix.
-        Since for the simulation only real valued matrices are used,
-        Sigma has to be symetric.
 
-    Nevertheless, Sigma (diffussion) matrix is calculated to check whether
+        Since for the simulation only real valued matrices are used,
+        Sigma has to be symetric (otherwise it could be Hermitian).
+
+        If Gamma is provided, Sigma is calculated to check whether
         it is positive definite (so that we get proper covariance matrices).
 
     Initial value of the process X(0) is sampled from bivariate normal
         distribution with mean mu and covariance matrix Gamma.
     """
 
-    def __init__(self, B, Gamma):
-        self.B = np.array(B)
-        self.Gamma = np.array(Gamma)
+    def __init__(self, B, Gamma=None, Sigma=None):
+        assert Gamma is not None or Sigma is not None, \
+            """You should provide either stationary covariance matrix (Gamma)
+            or diffusion matrix (Sigma)"""
+
+        # set and check B
         # B is assumed to be 2x2, real and have positive eigenvalues
+        self.B = np.array(B)
         assert np.all(np.isreal(self.B)), "B is not a real matrix"
         assert self.B.shape == (2, 2), "B is not 2x2 matrix"
         assert np.all(np.linalg.eigvals(self.B) >
                       0), "B has zero or negative eigenvalues"
-        # and Gamma are assumed to be 2x2, real, symmetric
-        # and positive semi-definite
-        # since it is symmertic, positive semidefiniteness is
-        # assured when eigenvalues are >= 0
-        assert np.all(np.isreal(self.Gamma)), "Gamma is not a real matrix"
-        assert self.Gamma.shape == (2, 2), "Gamma is not 2x2 matrix"
-        assert np.all(self.Gamma == self.Gamma.T), "Gamma not symetric"
-        assert np.all(np.linalg.eigvals(self.Gamma) >=
-                      0), "Gamma has negative eigenvalues"
-        # Sigma = B*Gamma + Gamma*B.T is positive-definite
-        self.Sigma = np.matmul(self.B, self.Gamma) + \
-            np.matmul(self.Gamma, self.B.T)
-        assert np.all(self.Sigma == self.Sigma.T) and \
-            np.all(np.linalg.eigvals(self.Sigma) > 0), \
-            "Diffusion matrix not possitive-definite"
+
+        # if Gamma is provided set and check it and set Sigma
+        # Gamma is assumed to be 2x2, real, symmetric and positive semi-definite
+        # Sigma = B*Gamma + Gamma*B.T should be positive-definite
+        # (symetric with positive eigenvalues)
+        if Gamma is not None and Sigma is None:
+            self.Gamma = np.array(Gamma)
+            assert np.all(np.isreal(self.Gamma)), "Gamma is not a real matrix"
+            assert self.Gamma.shape == (2, 2), "Gamma is not 2x2 matrix"
+            assert np.all(self.Gamma == self.Gamma.T), "Gamma not symetric"
+            assert np.all(np.linalg.eigvals(self.Gamma) >=
+                          0), "Gamma has negative eigenvalues"
+            # set Sigma
+            self.Sigma = np.matmul(self.B, self.Gamma) + \
+                np.matmul(self.Gamma, self.B.T)
+            assert np.all(self.Sigma == self.Sigma.T) and \
+                np.all(np.linalg.eigvals(self.Sigma) > 0), \
+                "Sigma not possitive-definite"
+
+        # if Sigma is provided, check it and set Gamma
+        # Sigma is assumed to be 2x2, symetric and positive semidefinite
+        # Sigma could be Hermitian, but for now simulation uses only real values
+        if Sigma is not None and Gamma is None:
+            self.Sigma = np.array(Sigma)
+            assert self.Sigma.shape == (2, 2), "Sigma is not 2x2 matrix"
+            assert np.all(self.Sigma == self.Sigma.T), "Sigma not symetric"
+            assert np.all(np.linalg.eigvals(self.Sigma) > 0), "Sigma has negative eigenvalues"
+            # set Gamma
+            # equation for Gamma is eq. 4.4.53 from
+            # https://archive.org/details/handbookofstocha0000gard/page/110/mode/2up
+            det_B = np.linalg.det(self.B)
+            tr_B = np.trace(self.B)
+            B_no_trace = self.B - tr_B*np.identity(2)
+            self.Gamma = (det_B*self.Sigma + np.matmul(np.matmul(B_no_trace, self.Sigma),
+                                                       B_no_trace.T)) / \
+                (2 * det_B * tr_B)
+            assert np.all(np.isreal(self.Gamma)), "Gamma is not a real matrix"
+            assert self.Gamma.shape == (2, 2), "Gamma is not 2x2 matrix"
+            assert np.all(self.Gamma == self.Gamma.T), "Gamma not symetric"
+            assert np.all(np.linalg.eigvals(self.Gamma) >=
+                          0), "Gamma has negative eigenvalues"
 
     def __str__(self):
         return """Two variable Ornstein–Uhlenbeck process;
@@ -101,8 +132,8 @@ class OU_process_2v(object):
         Uses timestep `d`, with default 0.1.
 
         The covariance is determined by the stochastic term of the
-        X(t) equation describing the process.
-        Cholesky decomposition of instantaneus covariance (difussion)
+        integral equation X(t) describing the process.
+        Cholesky decomposition of instantaneus covariance
         matrix appears in this equation. The conditional distribution
         of X(t) given X(t-d) is reparametrized so that it depends
         on B and Gamma.See for example oravecz2011hierarchical eq (5).
